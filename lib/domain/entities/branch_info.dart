84912802d431f65391e12a72112a63185dcc0dfa
loey92 <sunflare92@gmail.com>
2024-12-14 19:04:09 +0900

    refactoring: BranchInfo 분리

---
+++ lib/domain/entities/branch_info.dart
@@ -0,0 +1,21 @@
+import 'package:latlong2/latlong.dart';
+
+class BranchInfo {
+  LatLng point; // 모든 경로값
+  String description; // 각 분기의 경로값
+  double bearingToPoint; // 다음 경로까지의 방향값
+  bool crosswalk; // branch 좌표가 횡단보도면 true, 아니면 false
+  bool branch; // 해당 point가 branch 좌표라면 true, 아니면 false
+  BranchInfo(
+      // 생성자
+      this.point,
+      this.description,
+      this.bearingToPoint,
+      this.crosswalk,
+      this.branch);
+
+  @override
+  String toString() {
+    return 'BranchInfo{point: $point, : $branch, description: $description, bearingToBranch: $bearingToPoint, crosswalk: $crosswalk}';
+  }
+}
