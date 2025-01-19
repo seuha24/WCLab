00bf49f965f666b304efb8bca551fa345786636c
loey92-Mac Mini <loey92@sweech.io>
2025-01-19 18:12:03 +0900

    fix: 사용하지 않는 파일 삭제

--- lib/framework/bloc.dart
+++ lib/framework/bloc.dart
@@ -8,7 +8,3 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:safelight/framework/ui.dart';
 import 'package:sensors_plus/sensors_plus.dart';
-
-part '../presentation/bloc/accelerometer_bloc/accelerometer_event.dart';
-part '../presentation/bloc/accelerometer_bloc/accelerometer_state.dart';
-// part '../presentation/bloc/accelerometer_bloc/accelerometer_bloc.dart';
