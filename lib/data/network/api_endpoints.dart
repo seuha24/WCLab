a5feef14e4bc77775bae1aacd3b98157bc76a312
hojung <jack777998@gmail.com>
2025-07-16 11:49:40 +0900

    entrance_bloc 폴더명 변경

--- lib/data/network/api_endpoints.dart
+++ lib/data/network/api_endpoints.dart
@@ -1,8 +1,9 @@
 class ApiEndpoints {
   //static const String baseUrl = 'https://backend.catholicuniv.pillowstudio.kr';
-  static const String baseUrl = 'http://aws2.cuksl.xyz:3333';
-  static const String baseUrl2 = 'http://cuksl.xyz:3306';
-  static const String baseUrl3 = 'http://aws.cuksl.xyz:3333';
+  static const String baseUrl = 'http://aws2.cuksl.xyz:3333'; //로그인 서버
+  static const String baseUrl2 = 'http://cuksl.xyz:3306'; //연결 없음
+  //static const String baseUrl3 = 'http://aws.cuksl.xyz:3333'; //출입구 조회_aws
+  static const String baseUrl3 = 'http://cuksl.xyz:3306'; //출입구 조회_학교
 
   // Auth 관련 엔드포인트
   static const String googleAuthToken = '$baseUrl/auth/google/token';
@@ -16,10 +17,10 @@ class ApiEndpoints {
   // 출입구 관련 엔드포인트
 
   // 출입구 좌표 등록 (POST)
-  static const String saveBuildingEntrance = '$baseUrl2/save/entrances';
+  static const String saveBuildingEntrance = '$baseUrl3/save/entrances';
 
   /// 주소, 위도, 경도로 출입구 정보를 조회하는 엔드포인트 (GET)
   static String getBuildingEntrance(String encodedAddr, double longitude, double latitude) {
-    return '$baseUrl2/$encodedAddr/$longitude/$latitude';
+    return '$baseUrl3/$encodedAddr/$longitude/$latitude';
   }
 }
